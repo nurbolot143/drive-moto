9324f83875fb011328c8e7d84fe5ea7c3274e250
Nurbolot Boobekov <nurbolot.boobekov.pls@gmail.com>
2021-08-09 14:42:42 +0300

    drive-moto

---
+++ app/js/main.js
@@ -0,0 +1,62 @@
+$(function () {
+  //add active class
+  function active(element) {
+    element.forEach((link) => {
+      link.addEventListener("click", () => {
+        removeActiveClasses();
+        link.classList.add("active");
+      });
+    });
+
+    function removeActiveClasses() {
+      element.forEach((link) => {
+        link.classList.remove("active");
+      });
+    }
+  }
+
+  const headerBottomLinks = document.querySelectorAll(".header-menu-link");
+  const headerTopLinks = document.querySelectorAll(".navbar-link");
+  const searchButtons = document.querySelectorAll(".search-btn");
+  const productTabs = document.querySelectorAll(".product-tab");
+
+  active(headerTopLinks);
+  active(headerBottomLinks);
+  active(searchButtons);
+  active(productTabs);
+  //
+
+  //slider
+  $(".slider").slick({
+    prevArrow: '<button class="slider-btn slider-prev"></button>',
+    nextArrow: '<button class="slider-btn slider-next"></button>',
+    dots: true,
+  });
+  //
+
+  //search tab
+  $(".search-btn").click(function (e) {
+    e.preventDefault();
+
+    $(".search-form").removeClass("show");
+    $($(this).attr("href")).addClass("show");
+  });
+  //
+
+  //product like toggle
+  $(".product-like").click(function () {
+    $(this).toggleClass("show");
+  });
+  //
+
+  //product slider
+  $(".product-slider").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    prevArrow: ' <button class="product-btn product-prev"></button>',
+    nextArrow: '<button class="product-btn product-next"></button>',
+  });
+  //
+});
